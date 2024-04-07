3c29305bc545e8bd9c356c73a3e7bb0a97137f68
Wolvborg <binnu24668@gmail.com>
2024-04-07 20:23:10 +0530

    NUxt init
    Signed-off-by: Wolvborg <binnu24668@gmail.com>

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+import { NuxtLink } from '#build/components';
+<template>
+  <div>
+    <header>
+      <NuxtLink to="/">Home</NuxtLink> | <NuxtLink to="/about">About</NuxtLink>
+    </header>
+    <slot />
+    <footer>Footer</footer>
+  </div>
+</template>
