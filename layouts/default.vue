7f079628e631fb0a6985327319082be6ac609676
Wolvborg <binnu24668@gmail.com>
2024-04-07 20:33:05 +0530

    Prettier added
    Signed-off-by: Wolvborg <binnu24668@gmail.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,7 @@
 import { NuxtLink } from '#build/components';
 <template>
   <div>
-    <header>
-      <NuxtLink to="/">Home</NuxtLink> | <NuxtLink to="/about">About</NuxtLink>
-    </header>
+    <header><NuxtLink to="/">Home</NuxtLink> | <NuxtLink to="/about">About</NuxtLink></header>
     <slot />
     <footer>Footer</footer>
   </div>
